7bfe51b7559c4d89e64f0d30fccbe07897a28d62
 <macbook@s-MacBook-Pro.local>
2025-05-03 18:11:30 +0500

    QA Changes.

--- components/our-services/page.tsx
+++ components/our-services/page.tsx
@@ -43,15 +43,15 @@ const OurServices = () => {
                 index % 2 !== 0 ? "md:flex-row-reverse" : "md:flex-row"
               } items-center text-left`}
             >
-              <div className="lg:w-[55%] md:w-[55%] w-full h-full flex items-center">
+              <div className="lg:w-[55%] md:w-[55%] py-[40px] px-[40px] lg:px-[0px] md:px-[0px] w-full h-full flex items-center">
                 <div className="lg:w-[70%] md:w-[70%] w-full m-auto">
                   <h2 className="text-[24px] text-[#1A94D5] pb-[20px]">
                     Service
                   </h2>
-                  <h3 className="text-[64px] text-[#263F49] pb-[20px] font-bold">
+                  <h3 className="lg:text-[64px] md:text-[64px] text-[50px] text-[#263F49] pb-[20px] font-bold">
                     {item?.title}
                   </h3>
-                  <p className="text-[#606060] text-[32px]">
+                  <p className="text-[#606060] lg:text-[32px] md:text-[32px] text-[25px]">
                     {item?.description}
                   </p>
                   <Link href={item?.link}>
